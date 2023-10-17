fceded9fb9d15679294f4ed157d3c0bdb992d865
Abhinav <abhinav03031999@gmail.com>
2023-10-17 11:02:01 +1100

    Added Pie chart view - VIP Feature

--- src/views/DashboardView.java
+++ src/views/DashboardView.java
@@ -3,8 +3,10 @@ package views;
 import controllers.PostController;
 import controllers.UserController;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import models.User;
@@ -16,13 +18,13 @@ public class DashboardView {
     private PostController postController;
     private UserController userController;
     private Label welcomeMessage;
-    private Button addPostButton, viewPostsButton, editProfileButton, logoutButton, vipUpgradeButton;
+    private Button addPostButton, viewPostsButton, editProfileButton, logoutButton, vipUpgradeButton, dataVisualization, importPosts;
 
-    public DashboardView(User user, PostController postController, UserController userController) {
+    public DashboardView(Stage stage, User user, PostController postController, UserController userController) {
         this.user = user;
         this.postController = postController;
         this.userController = userController;
-        this.stage = new Stage();
+        this.stage = stage;
         initializeComponents();
     }
 
@@ -42,14 +44,23 @@ public class DashboardView {
         logoutButton.setOnAction(e -> handleLogout());
 
         if (!user.isVIP()) {
-            vipUpgradeButton = new Button("Upgrade to VIP");
+            vipUpgradeButton = new Button("VIP Features");
             vipUpgradeButton.setOnAction(e -> handleUpgradeToVIP());
+        } else {
+        	dataVisualization = new Button("Data Visualization");
+        	dataVisualization.setOnAction(e -> handleVisualization());
+        	
+        	importPosts = new Button("Import Posts");
+        	importPosts.setOnAction(e -> handleImports());
         }
 
         VBox layout = new VBox(10);
         layout.getChildren().addAll(welcomeMessage, addPostButton, viewPostsButton, editProfileButton, logoutButton);
         if (!user.isVIP()) {
             layout.getChildren().add(vipUpgradeButton);
+        } else {
+        	layout.getChildren().add(dataVisualization);
+        	layout.getChildren().add(importPosts);
         }
 
         stage.setScene(new Scene(layout, 300, 250));
@@ -57,45 +68,53 @@ public class DashboardView {
         stage.show();
     }
 
-    private void handleAddPost() {
+	private void handleAddPost() {
         // Switch to the PostFormView to allow the user to add a new post
     	System.out.println("DashboardView - User's ID: " + user.getUserId());
 
-        new PostFormView(postController, user);
-        stage.close();
+        new PostFormView(stage, postController, user);
+        //stage.close();
     }
 
     private void handleViewPosts() {
         // Display a list of the user's posts, with options to view, edit, or delete each post
-        new PostListView(postController, user);
-        stage.close();
+        new PostListView(stage, postController, user);
+        //stage.close();
     }
 
     private void handleEditProfile() {
         // Switch to ProfileView to allow the user to edit their profile details
-        new ProfileView(userController, user);
-        stage.close();
+        new ProfileView(stage, userController, user);
+        //stage.close();
     }
 
     private void handleLogout() {
         // Logout the user and return to the LoginView
-        new LoginView(stage, userController);
+        new LoginView(stage, userController, postController);
         //stage.close();
     }
 
     private void handleUpgradeToVIP() {
-        // Offer the user the option to upgrade to VIP status
-        if (userController.upgradeToVIP(user)) {
-            user.setVIP(true);
-            new DashboardView(user, postController, userController); // Refresh the dashboard
-            stage.close();
-        } else {
-            showError("Error upgrading to VIP. Please try again.");
-        }
+    	// Offer the user the option to upgrade to VIP status            
+        new UpgradeToVIPView(stage, user, userController, postController);
+           
     }
+    
+    private void handleVisualization() {
+		new PieChartView(stage, user, userController, postController);
+	}
+    
+    private void handleImports() {
+		new BulkImportView(stage, user, postController, userController);
+	}
 
     private void showError(String message) {
         // Display error message to user using a JavaFX Alert or similar
+    	 Alert alert = new Alert(AlertType.ERROR);
+         alert.setTitle("Error");
+         alert.setHeaderText(null);
+         alert.setContentText(message);
+         alert.showAndWait();
     }
 }
 
--- src/views/PieChartView.java
+++ src/views/PieChartView.java
@@ -3,8 +3,13 @@ package views;
 import java.util.List;
 
 import controllers.PostController;
+import controllers.UserController;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import models.Post;
 import models.User;
@@ -14,12 +19,15 @@ public class PieChartView {
     private Stage stage;
     private User user;
     private PostController postController;
+    private UserController userController;
     private PieChart sharesPieChart;
+    private Button backButton;
 
-    public PieChartView(PostController postController, User user) {
+    public PieChartView(Stage stage, User user, UserController userController, PostController postController) {
         this.postController = postController;
+        this.userController = userController;
         this.user = user;
-        this.stage = new Stage();
+        this.stage = stage;
         initializeComponents();
     }
 
@@ -45,11 +53,30 @@ public class PieChartView {
         PieChart.Data slice3 = new PieChart.Data("1000+ Shares", count1000Plus);
 
         sharesPieChart = new PieChart();
-        sharesPieChart.getData().add(slice1);
-        sharesPieChart.getData().add(slice2);
-        sharesPieChart.getData().add(slice3);
+        sharesPieChart.getData().addAll(slice1, slice2, slice3);
 
-        Scene scene = new Scene(sharesPieChart, 400, 400);
+        // Add percentages to pie chart slices
+        final ObservableList<PieChart.Data> chartData = sharesPieChart.getData();
+        chartData.forEach(data ->
+            data.nameProperty().bind(
+                javafx.beans.binding.Bindings.concat(
+                    data.getName(), " ", data.pieValueProperty(), " (",
+                    javafx.beans.binding.Bindings.format("%.1f%%",
+                        data.pieValueProperty().doubleValue() / chartData.stream().mapToDouble(PieChart.Data::getPieValue).sum() * 100),
+                    ")"
+                )
+            )
+        );
+
+        Label titleLabel = new Label("Posts Shares Distribution");
+        backButton = new Button("Back to Dashboard");
+        backButton.setOnAction(e -> {
+            // Return to the DashboardView
+            new DashboardView(stage, user, postController, userController);
+        });
+
+        VBox layout = new VBox(10, titleLabel, sharesPieChart, backButton);
+        Scene scene = new Scene(layout, 500, 500);
         stage.setScene(scene);
         stage.setTitle("Posts Shares Distribution");
         stage.show();
